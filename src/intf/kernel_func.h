d5d6f646d17ac2e0868cf744e16a87687478a145
Jeffrey Marvin <github@blitztiger.com>
2022-01-08 07:40:56 -0500

    Removed functions from kernel_func header that didn't belong

--- src/intf/kernel_func.h
+++ src/intf/kernel_func.h
@@ -6,9 +6,7 @@
 void print_mmap(multiboot_info_t* mbd);
 void kernel_init(multiboot_info_t* mbd);
 void kernel_panic(const char* message);
-void init_gdt();
 void init_page_frame_allocator(multiboot_info_t* mbd);
 void init_page_table_manager();
 void init_global_console(multiboot_info_t* mbd);
-void init_idt();
 void draw_time(uint8_t x, uint8_t y);
